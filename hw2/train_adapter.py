1c3beed73b49dcc4998c4e30e6e28a14448e47bd
cocr <forever.kzx0713@qq.com>
2024-11-07 00:39:46 +0800

    fix adapter's bug

--- hw2/train_adapter.py
+++ hw2/train_adapter.py
@@ -9,18 +9,20 @@ from transformers import (
     TrainingArguments,
     HfArgumentParser,
     DataCollatorWithPadding,
-    AdapterConfig
 )
 from transformers import set_seed
+from adapters import AutoAdapterModel, BnConfig
 import numpy as np
 import torch
 import wandb
 import os
 from typing import Optional
 from sklearn.metrics import f1_score, accuracy_score
-from adapters import AutoAdapterModel
 
 from dataHelper import get_dataset
+import os
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+os.environ["TORCH_USE_CUDA_DSA"] = "1"
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
@@ -34,11 +36,12 @@ logger = logging.getLogger(__name__)
 @dataclass
 class ModelArguments:
     model_name_or_path: str = field(metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"})
-    num_labels: int = field(default=13, metadata={"help": "Number of labels for classification"})
+    # res:3, acl:6, agn:4
+    num_labels: int = field(default=3, metadata={"help": "Number of labels for classification"})
 
 @dataclass
 class DataTrainingArguments:
-    dataset_name: str = field(metadata={"help": "Name of the dataset"})
+    dataset_name: list[str] = field(metadata={"help": "Name of the dataset"})
     max_seq_length: int = field(default=128, metadata={"help": "The maximum total input sequence length after tokenization."})
 
 @dataclass
@@ -48,17 +51,16 @@ class TrainingConfig:
     per_device_train_batch_size: int = field(default=8, metadata={"help": "Batch size per device during training"})
     per_device_eval_batch_size: int = field(default=8, metadata={"help": "Batch size for evaluation"})
     num_train_epochs: float = field(default=5.0, metadata={"help": "Total number of training epochs to perform."})
-    # seed: 42, 20, 11, 5, 2
+    # set seed: 42, 20, 13, 5, 2
     seed: int = field(default=42, metadata={"help": "Random seed for initialization"})
     report_to: str = field(default="wandb", metadata={"help": "Use Weights & Biases for tracking"})
-    run_name: str = field(default="bert_res_run1", metadata={"help": "Name of this WandB run"})
-    # run_name: str = field(default="roberta_base_run1", metadata={"help": "Name of this WandB run"})
-    # run_name: str = field(default="scibert_base_run1", metadata={"help": "Name of this WandB run"})
+    # run_name: bert_xxx_run1, roberta_xxx_run1, scibert_xxx_run1
+    run_name: str = field(default="adapter_res_run1", metadata={"help": "Name of this WandB run"})
     save_total_limit: int = field(default=1, metadata={"help": "Limit the total amount of checkpoints."})
     load_best_model_at_end: bool = field(default=True, metadata={"help": "Whether or not to load the best model found during training at the end."})
     metric_for_best_model: str = field(default="eval_loss", metadata={"help": "The metric to use to compare two different models."})
-    evaluation_strategy: str = field(default="steps", metadata={"help": "The evaluation strategy to adopt during training."})
-    save_strategy: str = field(default="steps", metadata={"help": "The checkpoint save strategy to adopt during training."})
+    evaluation_strategy: str = field(default="epoch", metadata={"help": "The evaluation strategy to adopt during training."})
+    save_strategy: str = field(default="epoch", metadata={"help": "The checkpoint save strategy to adopt during training."})
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
@@ -79,9 +81,7 @@ def compute_metrics(eval_pred):
 parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingConfig))
 model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 set_seed(training_args.seed)
-wandb.init(project="NLP_hw2_task2", name="Bert_res_run1")
-# wandb.init(project="NLP_hw2_task2", name="RoBerta_base_run1")
-# wandb.init(project="NLP_hw2_task2", name="SciBert_base_run1")
+wandb.init(project="NLP_hw2_task2", name=training_args.run_name)
 
 # load datasets
 print(f"Loading dataset {data_args.dataset_name}")
@@ -91,10 +91,15 @@ dataset = get_dataset(data_args.dataset_name, '<sep>')
 print(f"Loading model {model_args.model_name_or_path}")
 tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path, local_files_only=True)
 config = AutoConfig.from_pretrained(model_args.model_name_or_path, num_labels=model_args.num_labels, local_files_only=True)
-model = AutoModelForSequenceClassification.from_pretrained(model_args.model_name_or_path, config=config, local_files_only=True)
-adapter_config = AdapterConfig.load("pfeiffer", non_linearity="gelu", reduction_factor=16)
-model.load_adapter("AdapterHub/bert-base-uncased-pf-imdb", config=adapter_config)
-model.active_adapters = "bert-base-uncased-pf-imdb"
+# model = AutoModelForSequenceClassification.from_pretrained(model_args.model_name_or_path, config=config, local_files_only=True)
+
+model = AutoAdapterModel.from_pretrained(model_args.model_name_or_path, config=config, local_files_only=True)
+adapter_name = "res_adapter"
+config = BnConfig(mh_adapter=True, output_adapter=True, reduction_factor=16, non_linearity="relu")
+model.add_adapter(adapter_name, config=config)
+model.add_classification_head(adapter_name, num_labels=model_args.num_labels, activation_function="relu")
+model.train_adapter(adapter_name)
+
 for name, param in model.named_parameters():
     if not param.data.is_contiguous():
         param.data = param.data.contiguous()
